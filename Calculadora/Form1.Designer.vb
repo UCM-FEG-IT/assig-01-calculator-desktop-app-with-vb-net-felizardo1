4db39050180dad051b5b3a03958f6f418d301939
felizardo1 <48018680+felizardo1@users.noreply.github.com>
2019-02-28 13:11:03 +0200

    calculadora
    Felizardo

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,255 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.numero8 = New System.Windows.Forms.Button()
+        Me.numero7 = New System.Windows.Forms.Button()
+        Me.numero9 = New System.Windows.Forms.Button()
+        Me.numero4 = New System.Windows.Forms.Button()
+        Me.back = New System.Windows.Forms.Button()
+        Me.numero1 = New System.Windows.Forms.Button()
+        Me.subtracao = New System.Windows.Forms.Button()
+        Me.numero6 = New System.Windows.Forms.Button()
+        Me.numero5 = New System.Windows.Forms.Button()
+        Me.adi = New System.Windows.Forms.Button()
+        Me.numero3 = New System.Windows.Forms.Button()
+        Me.numero2 = New System.Windows.Forms.Button()
+        Me.iqual = New System.Windows.Forms.Button()
+        Me.ponto = New System.Windows.Forms.Button()
+        Me.multi = New System.Windows.Forms.Button()
+        Me.numero0 = New System.Windows.Forms.Button()
+        Me.TextBox1_Text = New System.Windows.Forms.TextBox()
+        Me.div = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'numero8
+        '
+        Me.numero8.Location = New System.Drawing.Point(74, 79)
+        Me.numero8.Name = "numero8"
+        Me.numero8.Size = New System.Drawing.Size(51, 23)
+        Me.numero8.TabIndex = 4
+        Me.numero8.Text = "8"
+        Me.numero8.UseVisualStyleBackColor = True
+        '
+        'numero7
+        '
+        Me.numero7.Location = New System.Drawing.Point(21, 79)
+        Me.numero7.Name = "numero7"
+        Me.numero7.Size = New System.Drawing.Size(47, 23)
+        Me.numero7.TabIndex = 5
+        Me.numero7.Text = "7"
+        Me.numero7.UseVisualStyleBackColor = True
+        '
+        'numero9
+        '
+        Me.numero9.Location = New System.Drawing.Point(131, 79)
+        Me.numero9.Name = "numero9"
+        Me.numero9.Size = New System.Drawing.Size(46, 23)
+        Me.numero9.TabIndex = 6
+        Me.numero9.Text = "9"
+        Me.numero9.UseVisualStyleBackColor = True
+        '
+        'numero4
+        '
+        Me.numero4.Location = New System.Drawing.Point(21, 109)
+        Me.numero4.Name = "numero4"
+        Me.numero4.Size = New System.Drawing.Size(47, 23)
+        Me.numero4.TabIndex = 7
+        Me.numero4.Text = "4"
+        Me.numero4.UseVisualStyleBackColor = True
+        '
+        'back
+        '
+        Me.back.Location = New System.Drawing.Point(183, 79)
+        Me.back.Name = "back"
+        Me.back.Size = New System.Drawing.Size(66, 23)
+        Me.back.TabIndex = 8
+        Me.back.Text = "C"
+        Me.back.UseVisualStyleBackColor = True
+        '
+        'numero1
+        '
+        Me.numero1.Location = New System.Drawing.Point(21, 138)
+        Me.numero1.Name = "numero1"
+        Me.numero1.Size = New System.Drawing.Size(47, 23)
+        Me.numero1.TabIndex = 9
+        Me.numero1.Text = "1"
+        Me.numero1.UseVisualStyleBackColor = True
+        '
+        'subtracao
+        '
+        Me.subtracao.Location = New System.Drawing.Point(183, 109)
+        Me.subtracao.Name = "subtracao"
+        Me.subtracao.Size = New System.Drawing.Size(67, 23)
+        Me.subtracao.TabIndex = 10
+        Me.subtracao.Text = "-"
+        Me.subtracao.UseVisualStyleBackColor = True
+        '
+        'numero6
+        '
+        Me.numero6.Location = New System.Drawing.Point(130, 109)
+        Me.numero6.Name = "numero6"
+        Me.numero6.Size = New System.Drawing.Size(47, 23)
+        Me.numero6.TabIndex = 11
+        Me.numero6.Text = "6"
+        Me.numero6.UseVisualStyleBackColor = True
+        '
+        'numero5
+        '
+        Me.numero5.Location = New System.Drawing.Point(74, 109)
+        Me.numero5.Name = "numero5"
+        Me.numero5.Size = New System.Drawing.Size(51, 23)
+        Me.numero5.TabIndex = 12
+        Me.numero5.Text = "5"
+        Me.numero5.UseVisualStyleBackColor = True
+        '
+        'adi
+        '
+        Me.adi.Location = New System.Drawing.Point(182, 138)
+        Me.adi.Name = "adi"
+        Me.adi.Size = New System.Drawing.Size(67, 23)
+        Me.adi.TabIndex = 13
+        Me.adi.Text = "+"
+        Me.adi.UseVisualStyleBackColor = True
+        '
+        'numero3
+        '
+        Me.numero3.Location = New System.Drawing.Point(130, 138)
+        Me.numero3.Name = "numero3"
+        Me.numero3.Size = New System.Drawing.Size(47, 23)
+        Me.numero3.TabIndex = 14
+        Me.numero3.Text = "3"
+        Me.numero3.UseVisualStyleBackColor = True
+        '
+        'numero2
+        '
+        Me.numero2.Location = New System.Drawing.Point(74, 138)
+        Me.numero2.Name = "numero2"
+        Me.numero2.Size = New System.Drawing.Size(51, 23)
+        Me.numero2.TabIndex = 15
+        Me.numero2.Text = "2"
+        Me.numero2.UseVisualStyleBackColor = True
+        '
+        'iqual
+        '
+        Me.iqual.Location = New System.Drawing.Point(182, 167)
+        Me.iqual.Name = "iqual"
+        Me.iqual.Size = New System.Drawing.Size(66, 40)
+        Me.iqual.TabIndex = 19
+        Me.iqual.Text = "="
+        Me.iqual.UseVisualStyleBackColor = True
+        '
+        'ponto
+        '
+        Me.ponto.Location = New System.Drawing.Point(74, 167)
+        Me.ponto.Name = "ponto"
+        Me.ponto.Size = New System.Drawing.Size(38, 40)
+        Me.ponto.TabIndex = 18
+        Me.ponto.Text = "."
+        Me.ponto.UseVisualStyleBackColor = True
+        '
+        'multi
+        '
+        Me.multi.Location = New System.Drawing.Point(118, 167)
+        Me.multi.Name = "multi"
+        Me.multi.Size = New System.Drawing.Size(27, 40)
+        Me.multi.TabIndex = 17
+        Me.multi.Text = "*"
+        Me.multi.UseVisualStyleBackColor = True
+        '
+        'numero0
+        '
+        Me.numero0.Location = New System.Drawing.Point(21, 167)
+        Me.numero0.Name = "numero0"
+        Me.numero0.Size = New System.Drawing.Size(47, 40)
+        Me.numero0.TabIndex = 16
+        Me.numero0.Text = "0"
+        Me.numero0.UseVisualStyleBackColor = True
+        '
+        'TextBox1_Text
+        '
+        Me.TextBox1_Text.Location = New System.Drawing.Point(21, 33)
+        Me.TextBox1_Text.MaximumSize = New System.Drawing.Size(330, 30)
+        Me.TextBox1_Text.MinimumSize = New System.Drawing.Size(4, 35)
+        Me.TextBox1_Text.Name = "TextBox1_Text"
+        Me.TextBox1_Text.Size = New System.Drawing.Size(227, 20)
+        Me.TextBox1_Text.TabIndex = 30
+        '
+        'div
+        '
+        Me.div.Location = New System.Drawing.Point(149, 167)
+        Me.div.Name = "div"
+        Me.div.Size = New System.Drawing.Size(27, 40)
+        Me.div.TabIndex = 31
+        Me.div.Text = "/"
+        Me.div.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 291)
+        Me.Controls.Add(Me.div)
+        Me.Controls.Add(Me.TextBox1_Text)
+        Me.Controls.Add(Me.iqual)
+        Me.Controls.Add(Me.ponto)
+        Me.Controls.Add(Me.multi)
+        Me.Controls.Add(Me.numero0)
+        Me.Controls.Add(Me.numero2)
+        Me.Controls.Add(Me.numero3)
+        Me.Controls.Add(Me.adi)
+        Me.Controls.Add(Me.numero5)
+        Me.Controls.Add(Me.numero6)
+        Me.Controls.Add(Me.subtracao)
+        Me.Controls.Add(Me.numero1)
+        Me.Controls.Add(Me.back)
+        Me.Controls.Add(Me.numero4)
+        Me.Controls.Add(Me.numero9)
+        Me.Controls.Add(Me.numero7)
+        Me.Controls.Add(Me.numero8)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents numero8 As Button
+    Friend WithEvents numero7 As Button
+    Friend WithEvents numero9 As Button
+    Friend WithEvents numero4 As Button
+    Friend WithEvents back As Button
+    Friend WithEvents numero1 As Button
+    Friend WithEvents subtracao As Button
+    Friend WithEvents numero6 As Button
+    Friend WithEvents numero5 As Button
+    Friend WithEvents adi As Button
+    Friend WithEvents numero3 As Button
+    Friend WithEvents numero2 As Button
+    Friend WithEvents iqual As Button
+    Friend WithEvents ponto As Button
+    Friend WithEvents multi As Button
+    Friend WithEvents numero0 As Button
+    Friend WithEvents TextBox1_Text As TextBox
+    Friend WithEvents div As Button
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles numero3.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles ponto.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles subtracao.Click
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles numero0.Click
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles numero2.Click
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles adi.Click
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles iqual.Click
+    End Sub
+
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles div.Click
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles numero6.Click
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles back.Click
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles numero1.Click
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles numero0.Click, numero1.Click, numero2.Click, numero3.Click, numero4.Click, numero5.Click, numero6.Click, numero7.Click, numero8.Click, numero9.Click, ponto.Click, back.Click, div.Click, iqual.Click, multi.Click, adi.Click, subtracao.Click
+        Dim button As Button = CType(sender, Button)
+        If button.Name = "numero1" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "1"
+        End If
+
+        If button.Name = "numero2" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "2"
+        End If
+
+        If button.Name = "numero3" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "3"
+        End If
+
+        If button.Name = "numero4" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "4"
+        End If
+
+        If button.Name = "numero5" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "5"
+        End If
+
+        If button.Name = "numero6" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "6"
+        End If
+
+        If button.Name = "numero7" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "7"
+        End If
+
+        If button.Name = "numero8" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "8"
+        End If
+
+        If button.Name = "numero9" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "9"
+        End If
+
+        If button.Name = "numero0" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "0"
+        End If
+
+        If button.Name = "ponto" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "."
+        End If
+
+        If button.Name = "iqual" Then
+            Dim equation As String = TextBox1_Text.Text
+            Dim result = New DataTable().Compute(equation, Nothing)
+            TextBox1_Text.Text = result
+        End If
+
+        If button.Name = "multi" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "*"
+        End If
+
+        If button.Name = "div" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "/"
+        End If
+
+        If button.Name = "adi" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "+"
+        End If
+
+        If button.Name = "subtracao" Then
+
+            TextBox1_Text.Text = TextBox1_Text.Text + "-"
+        End If
+
+        If button.Name = "back" Then
+
+            TextBox1_Text.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles numero4.Click
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles numero9.Click
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles numero7.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles numero8.Click
+
+    End Sub
+
+    Private Sub div_Click(sender As Object, e As EventArgs) Handles div.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1_Text.TextChanged
+
+    End Sub
+End Class
